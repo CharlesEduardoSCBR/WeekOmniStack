07cfcfad21f6ccaaf5a54797b4a91d64a7943711
Charles Eduardo <charles.eduardo.sc.br@gmail.com>
2019-08-07 19:45:04 -0300

    controler...

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,8 +1,20 @@
+const axios = require('axios');
+const Dev = require('../models/Dev')
+
 module.exports = {
-    store(req, res) {
+    async store(req, res) {
 
         const { username } = req.body;
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        const { name, bio, avatar_url: avatar } = response.data;
 
-        return res.json({ ok: true });
+        const dev = await Dev.create({
+            name,
+            user : username,
+            bio,
+            avatar
+        });
+
+        return res.json(dev);
     }
 };
