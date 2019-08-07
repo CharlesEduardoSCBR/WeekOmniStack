c8ac5f454e77f71a89bcd9b16c7685810cec086a
Charles Eduardo <charles.eduardo.sc.br@gmail.com>
2019-08-06 21:08:18 -0300

    Parte configuração inicial da conexao, done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://CharlesEdu:re.pe_5c@cluster0-3wpcc.mongodb.net/omnistack8?retryWrites=true&w=majority', { useNewUrlParser: true });
 
 server.use(express.json());
 server.use(routes);
