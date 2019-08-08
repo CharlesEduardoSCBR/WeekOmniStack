9ef1e67a5518a32b47e6825449fc6e098695aaf4
Charles Eduardo <charles.eduardo.sc.br@gmail.com>
2019-08-07 22:51:58 -0300

    add cors....

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
@@ -6,6 +7,7 @@ const server = express();
 
 mongoose.connect('mongodb+srv://CharlesEdu:re.pe_5c@cluster0-3wpcc.mongodb.net/omnistack8?retryWrites=true&w=majority', { useNewUrlParser: true });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 server.listen(3333);
