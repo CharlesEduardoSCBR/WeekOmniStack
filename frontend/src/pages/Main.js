9a9a18d6683078d70c9d7a18a633dfee2393e3d0
Charles Eduardo <charles.eduardo.sc.br@gmail.com>
2019-08-09 14:22:47 -0300

    Incrementando e estilizando a pagina Main

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,36 @@
 import React from 'react';
 
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
+import './Main.css';
+
 export default function Main({ match }) {
-    return <h1>{match.params.id}</h1>
+    return (
+    	<div className="main-container">
+    		<img src={logo} alt="Tindev" />
+
+    		<ul>
+    			<li>
+    				<img src="https://avatars2.githubusercontent.com/u/" alt="NomeDev" />
+    				<footer>
+    					<strong>Nome</strong>
+    					<p>Lorum Ipsum Description</p>
+    				</footer>
+
+    				<div className="buttons">
+    					<button type="buton">
+    						<img src={like} alt="Like" />
+    					</button>
+
+    					<button type="buton">
+    						<img src={dislike} alt="Dislike" />
+    					</button>
+    				</div>
+
+    			</li>
+    		</ul>
+    	</div>
+    )
 }
