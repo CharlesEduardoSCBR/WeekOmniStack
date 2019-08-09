8896bee901fb56eefcbfae5fc145af139ba1ad6e
Charles Eduardo <charles.eduardo.sc.br@gmail.com>
2019-08-09 14:42:59 -0300

    Adicionado o evento para leitura (render) da lista

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
@@ -7,29 +9,53 @@ import dislike from '../assets/dislike.svg';
 import './Main.css';
 
 export default function Main({ match }) {
+
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => 
+		{
+			async function loadUsers(){
+				const response = await api.get(
+									'/devs', 
+									{
+										headers: {
+											user: match.params.id,
+										}
+									}
+								 )
+
+				setUsers(response.data);
+			}
+
+			loadUsers();
+		}, [match.params.id]
+	);
+
     return (
     	<div className="main-container">
     		<img src={logo} alt="Tindev" />
 
     		<ul>
-    			<li>
-    				<img src="https://avatars2.githubusercontent.com/u/" alt="NomeDev" />
-    				<footer>
-    					<strong>Nome</strong>
-    					<p>Lorum Ipsum Description</p>
-    				</footer>
 
-    				<div className="buttons">
-    					<button type="buton">
-    						<img src={like} alt="Like" />
-    					</button>
+    			{users.map(user => (
+	   				<li key={user._id}>
+    					<img src={user.avatar} alt={user.name} />
+    					<footer>
+    						<strong>{user.name}</strong>
+    						<p>{user.bio}</p>
+    					</footer>
 
-    					<button type="buton">
-    						<img src={dislike} alt="Dislike" />
-    					</button>
-    				</div>
+    					<div className="buttons">
+    						<button type="buton">
+    							<img src={like} alt="Like" />
+    						</button>
 
-    			</li>
+    						<button type="buton">
+    							<img src={dislike} alt="Dislike" />
+    						</button>
+    					</div>
+    				</li>
+    				))}
     		</ul>
     	</div>
     )
