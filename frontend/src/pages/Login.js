458766001e14e496158001e08e4a6ce732ce5288
Charles Eduardo <charles.eduardo.sc.br@gmail.com>
2019-08-08 23:02:00 -0300

    Add paginas Login e Main

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Login.css';
 
 import logo from '../assets/logo.svg';
 
 export default function Login() {
+    const [username, setUsername] = useState('');
+
     return ( 
             <div className = "login-container" >
-                <form >
+                <form>
                     <img src = { logo } alt = "Tindev" / >
-                    <input placeholder = "Digite seu usuário no GitHub" / >
-                    <button type = "submit" > Enviar </button> 
+                    <input 
+                        placeholder = "Digite seu usuário no GitHub" 
+                        value = {username}
+                        onChange = {e => setUsername(e.target.value)}
+                    />
+                    <button type = "submit"> Enviar </button> 
                 </form>
             </div>
     );
